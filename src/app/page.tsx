3c0d19802082c7a294d3916c930ab7820f30c109
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-10-25 18:49:50 +0530

    homepage created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import ExploreBtn from "@/src/components/ExploreBtn";
+import EventCard from "@/src/components/EventCard";
+import events from "@/src/lib/constants";
+
 const Page = () => {
 	return (
 		<section>
@@ -7,6 +11,20 @@ const Page = () => {
 			<p className="text-center mt-5">
 				Hackathons, Meetups and Conferences, All in One Place
 			</p>
+
+			<ExploreBtn />
+
+			<div className="mt-20 space-y-7">
+				<h3>Featured Events</h3>
+
+				<ul className="events list-none">
+					{events.map((event) => (
+						<li key={event.title}>
+							<EventCard {...event} />
+						</li>
+					))}
+				</ul>
+			</div>
 		</section>
 	);
 };
