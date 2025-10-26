c71fc8b71d7934c52968411014ca6c32de0ab8a4
Aniket Kumar Mahato <aniketkumar0x@gmail.com>
2025-10-26 19:34:41 +0530

    final fix (hopefully)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,15 @@ import { cacheLife } from "next/cache";
 import ExploreBtn from "@/src/components/ExploreBtn";
 import EventCard from "@/src/components/EventCard";
 import { IEvent } from "@/src/database";
-import events from "@/src/lib/constants";
 
-// const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const Page = async () => {
 	"use cache";
 	cacheLife("hours");
 
-	// const response = await fetch(`${BASE_URL}/api/events`);
-	// const { events } = await response.json();
+	const response = await fetch(`${BASE_URL}/api/events`);
+	const { events } = await response.json();
 
 	return (
 		<section>
@@ -30,7 +29,7 @@ const Page = async () => {
 				<ul className="events list-none">
 					{events &&
 						events.length > 0 &&
-						events.map((event) => (
+						events.map((event: IEvent) => (
 							<li key={event.title}>
 								<EventCard {...event} />
 							</li>
